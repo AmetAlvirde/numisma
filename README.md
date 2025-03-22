a4e0f2b17ef82c20cb784193aa52052b941258a0
Amet Alvirde <Amet.Alvirde@gmail.com>
2025-03-21 23:32:10 -0300

    Painful project reset to migrate to Next.
     But this will get us to an MVP faster

--- README.md
+++
@@ -1,4 +0,0 @@
-# Numisma
-
-An app that makes easier to track investment and trading portfolios and
-strategies
--- app/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Numisma",
-  description: "Your digital coin collection manager",
-};
