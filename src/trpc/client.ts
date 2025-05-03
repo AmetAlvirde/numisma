f6a05bcf556149af87e5564458ed84b879328a3c
Amet Alvirde <Amet.Alvirde@gmail.com>
2025-05-03 11:38:47 -0300

    feature: Updates deprecated tRPC methods
    Adds documentation
    Fixes .vscode location for tailwind file association

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,9 +1,9 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "@/server/trpc/router";
 import superjson from "superjson";
 import { getBaseUrl } from "@/utils/getBaseUrl";
 
-export const trpc = createTRPCProxyClient<AppRouter>({
+export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: `${getBaseUrl()}/api/trpc`,
