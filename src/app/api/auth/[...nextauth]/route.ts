12819228e711f88b43b798a86a658d350fbf7c42
Amet Alvirde <Amet.Alvirde@gmail.com>
2026-05-19 19:13:57 -0400

    Resets to start over again

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/utils/auth";
-
-// Export handlers for GET and POST requests
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
